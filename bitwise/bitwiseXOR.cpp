f6996d90754db6c641aa480f69dc4b84c123d07d
iotBrainStorm <www.mrinal.phsa@gmail.com>
2026-07-18 20:18:37 +0530

    bitwise XOR

---
+++ bitwise/bitwiseXOR.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b;
+  cout << "please enter two numbers for XOR operation: ";
+  cin >> a >> b;
+
+  cout << a << " XOR " << b << " is: " << (a ^ b) << endl;
+
+  return 0;
+}
