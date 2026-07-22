d45ba01b5ca14082a91b93acee0a2e69381e8820
iotBrainStorm <www.mrinal.phsa@gmail.com>
2026-07-22 08:59:30 +0530

    pass by reference using alias

--- pointers/passByRef.cpp
+++ pointers/passByRef.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 void passByValue(int a) { a = 20; }
 
-void passByRef(int* a) { *a = 20; }
+void passByRef(int* a) { *a = 20; }  // using pointer
+
+void passByRefAlias(int& b) { b = 20; }  // using alias
 
 int main() {
   int a = 10;
@@ -14,7 +16,10 @@ int main() {
   cout << "pass by value is: " << a << endl;
 
   passByRef(&a);
-  cout << "pass by reference is: " << a << endl;
+  cout << "pass by reference using pointer is: " << a << endl;
+
+  passByRefAlias(a);
+  cout << "pass by reference using alias is: " << a << endl;
 
   return 0;
 }
