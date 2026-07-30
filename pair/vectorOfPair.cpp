4dc4e22e8f6f0b9d4bd4506685f0d65498473858
iotBrainStorm <www.mrinal.phsa@gmail.com>
2026-07-30 08:52:37 +0530

    vector of pair push_back(), STL

--- pair/vectorOfPair.cpp
+++ pair/vectorOfPair.cpp
@@ -11,5 +11,13 @@ int main() {
   for (auto p : vec) {
     cout << p.first << " " << p.second << endl;
   }
+
+  // push back
+  vec.push_back({'e', 15});
+  vec.emplace_back('f', 17);  // in-place object create
+  for (auto p : vec) {
+    cout << p.first << " " << p.second << endl;
+  }
+
   return 0;
 }
