9a32e8a1cdffcb865f17ac74b99321f206faad1f
iotBrainStorm <www.mrinal.phsa@gmail.com>
2026-07-30 09:07:00 +0530

    pop (remove from last) in stack, STL

--- stack/basicStack.cpp
+++ stack/basicStack.cpp
@@ -3,12 +3,22 @@
 using namespace std;
 
 int main() {
+  /* stack always works on Last in First out method */
   stack<int> s;
 
   // push in stack
   s.push(1);
+  s.push(5);
+  s.push(4);
+  s.push(3);
+  s.push(8);
+  s.push(7);
   s.push(-4);
   cout << "top value: " << s.top() << endl;  // -4
+
+  // delete element from stack
+  s.pop();
+  cout << "top value after pop: " << s.top() << endl;  // 7
 
   return 0;
 }
