fc56f5cd5facefb9f2b8cb01fa298d13de9fa94f
iotBrainStorm <www.mrinal.phsa@gmail.com>
2026-07-30 08:30:09 +0530

    print the pair values, STL

---
+++ pair/basicPair.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  pair<int, int> p = {2, 3};
+  pair<string, int> pair = {"Mrinal", 1997};
+
+  // print the pair
+  cout << pair.first << endl;
+  cout << pair.second << endl;
+
+  return 0;
+}
