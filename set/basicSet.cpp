0e16829a5583b0d683a28107534dcd5c790e230d
iotBrainStorm <www.mrinal.phsa@gmail.com>
2026-07-31 09:41:42 +0530

    upper bound in set, STL

--- set/basicSet.cpp
+++ set/basicSet.cpp
@@ -8,6 +8,7 @@ int main() {
    * it stores the data in sorted order
    * if we lower bound function it will the exactly same value if it exist
    * otherwise it will be return just immediate greater value
+   * if we upper bound function it will return just immediate greater value
    */
 
   set<int> s;
@@ -32,5 +33,8 @@ int main() {
   cout << "lower bound (5): " << *(s.lower_bound(5)) << endl;    // 5
   cout << "lower bound (7): " << *(s.lower_bound(7)) << endl;    // 9
   cout << "lower bound (12): " << *(s.lower_bound(12)) << endl;  // garbage
+
+  // upper bound
+  cout << "upper bound (0): " << *(s.upper_bound(0)) << endl;  // 1
   return 0;
 }
