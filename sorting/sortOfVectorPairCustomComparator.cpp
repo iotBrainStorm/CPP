c448822f4d1671d00cf837994404375c46d31db2
iotBrainStorm <www.mrinal.phsa@gmail.com>
2026-08-01 08:05:30 +0530

    sort of vector pair using custom comparator case 2, ascending

--- sorting/sortOfVectorPairCustomComparator.cpp
+++ sorting/sortOfVectorPairCustomComparator.cpp
@@ -10,9 +10,20 @@ bool comparator(pair<int, int> p1, pair<int, int> p2) {
     return false;
 }
 
+bool comparatorCase2(pair<int, int> p1, pair<int, int> p2) {
+  if (p1.second < p2.second)
+    return true;
+  if (p1.second > p2.second)
+    return false;
+
+  if (p1.first < p2.first)
+    return true;
+  else
+    return false;
+}
+
 int main() {
-  vector<pair<int, int>> vec = {{3, 5}, {1, 8},   {-7, 5}, {-1, -9},
-                                {0, 0}, {12, 15}, {-90, 5}};
+  vector<pair<int, int>> vec = {{3, 5}, {1, 8}, {-7, 5}, {-1, -9}, {0, 0}, {12, 15}, {-90, 5}};
   cout << "original vector is like:\n";
   for (auto p : vec) {
     cout << p.first << " " << p.second << endl;
@@ -21,6 +32,13 @@ int main() {
   // ascending order sort on the basis second element of pair
   sort(vec.begin(), vec.end(), comparator);
   cout << "ascending order sort on the basis second element of pair:\n";
+  for (auto p : vec) {
+    cout << p.first << " " << p.second << endl;
+  }
+
+  // ascending order sort on the basis second and first element of pair
+  sort(vec.begin(), vec.end(), comparatorCase2);
+  cout << "ascending order sort on the basis second and first element of pair:\n";
   for (auto p : vec) {
     cout << p.first << " " << p.second << endl;
   }
