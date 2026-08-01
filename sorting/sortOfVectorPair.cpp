39446a96164e2fcb7a29323fd6a1a9f82a39d7d2
iotBrainStorm <www.mrinal.phsa@gmail.com>
2026-08-01 07:39:14 +0530

    sort of vector pair on the basis first element of pair

---
+++ sorting/sortOfVectorPair.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  vector<pair<int, int>> vec = {{3, 5}, {1, 8},   {-7, 5}, {-1, -9},
+                                {0, 0}, {12, 15}, {-90, 5}};
+  cout << "original vector is like:\n";
+  for (auto p : vec) {
+    cout << p.first << " " << p.second << endl;
+  }
+
+  // ascending order sort on the basis first element of pair
+  sort(vec.begin(), vec.end());
+  cout << "ascending order sort on the basis first element of pair:\n";
+  for (auto p : vec) {
+    cout << p.first << " " << p.second << endl;
+  }
+
+  return 0;
+}
