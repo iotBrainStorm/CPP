0f6959ecdd513be1e2423cb2df85a308eb15d991
iotBrainStorm <www.mrinal.phsa@gmail.com>
2026-08-05 08:11:07 +0530

    shortcut to find out the count of digits in a number

--- maths/digitsInNumber.cpp
+++ maths/digitsInNumber.cpp
@@ -1,12 +1,6 @@
-/*
- * sieve of eratosthenes process
- * here we will find all the prime numbers in a given range
- * in this process, first we assume all numbers in given range is prime => assign true (here 0, 1 will be skipped)
- * next we will travel all the numbers and its factors also => assign false
- */
-
 #include <algorithm>
 #include <iostream>
+#include <math.h>
 #include <vector>
 using namespace std;
 
@@ -35,6 +29,7 @@ int main() {
   }
   cout << endl;
   cout << "number of digits: " << ans.size() << endl;
+  cout << "number of digits: " << (int)(log10(number) + 1) << endl; // shortcut to find out the number of digits
 
   return 0;
 }
