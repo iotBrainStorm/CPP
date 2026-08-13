2d0d490331485f56af00ed4ddafcec8e2a7061c2
iotBrainStorm <www.mrinal.phsa@gmail.com>
2026-08-13 07:28:49 +0530

    basic recursion

---
+++ recursion/basicRecursion.cpp
@@ -0,0 +1,27 @@
+/*
+ * when a function calls itself
+ * base case => when satisfied the ending condition
+ */
+
+#include <iostream>
+using namespace std;
+
+void printNums(int num) {
+
+  if (num == 1) {
+    cout << "1" << endl;
+    return;
+  }
+
+  cout << num << " ";
+  num--;
+  printNums(num);
+}
+
+int main() {
+  int range;
+  cout << "please entr your range to print: ";
+  cin >> range;
+  printNums(range);
+  return 0;
+}
